eab9a940982e4e2be69b92860131db6ea433c46c
ayat kalybekova <kalybekAyat@outlook.com>
2025-02-06 11:19:20 +0600

    structure

--- src/appPages/site/components/layout/LayoutSite.tsx
+++ src/appPages/site/components/layout/LayoutSite.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+"use client";
+import { FC, ReactNode } from "react";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 
-const LayoutSite = () => {
-  return <div>LayoutSite</div>;
+interface IProps {
+  children: ReactNode;
+}
+
+const LayoutSite: FC<IProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
 };
 
 export default LayoutSite;
