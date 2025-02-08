10a240fede2dc19d957b6126361c0ff9c809b952
ayat kalybekova <kalybekAyat@outlook.com>
2025-02-08 23:11:38 +0600

    update sign-in

--- src/appPages/site/components/pages/Home.tsx
+++ src/appPages/site/components/pages/Home.tsx
@@ -1,6 +1,10 @@
+"use client";
+import { useUserQuery } from "@/redux/api/auth";
 import React from "react";
 
 const Home = () => {
+  const { data } = useUserQuery();
+  console.log("🚀 ~ Header ~ data:", data);
   return <div>HomePage 1234567890</div>;
 };
 
--- types/schema.d.ts
+++ types/schema.d.ts
@@ -2,3 +2,20 @@ interface LogIn {
   username: string;
   password: string;
 }
+
+interface Register {
+  username: string;
+  email: string;
+  password: string;
+  first_name: string;
+  last_name: string;
+  phone_number: string;
+  status_acc: string;
+  age: string;
+}
+
+interface UserList {
+  id: number;
+  username: string;
+  profile_picture: string;
+}
