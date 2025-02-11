49aceba1cb86e0971ddfff93ac5e4dcb2d36a35a
ayat kalybekova <kalybekAyat@outlook.com>
2025-02-11 18:57:52 +0600

    chenges

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <section>
+      <div className="container">NotFound</div>
+    </section>
+  );
+};
+
+export default NotFound;
--- src/appPages/site/components/pages/Home.tsx
+++ src/appPages/site/components/pages/Home.tsx
@@ -1,42 +1,14 @@
 "use client";
-import { useLogOutMutation, useUserQuery } from "@/redux/api/auth";
-import React from "react";
-import { useRouter } from "next/navigation";
-import SideBar from "./SideBar/SideBar";
+import MyUser from "./myUser/MyUser";
+import s from "./Home.module.scss";
 
 const Home = () => {
-  const [logoutUser] = useLogOutMutation();
-  const router = useRouter();
-
-  const handleLogout = async () => {
-    const tokens = localStorage.getItem("tokens");
-    if (!tokens) {
-      console.error("Ошибка: нет токенов в localStorage.");
-      return;
-    }
-
-    const accessToken = JSON.parse(tokens).access;
-
-    try {
-      await logoutUser({ access: accessToken }).unwrap(); // Отправляешь токен на сервер
-      localStorage.removeItem("tokens");
-      router.push("/"); // Перенаправление на страницу логина
-    } catch (error) {
-      console.error("Ошибка при выходе:", error);
-    }
-  };
-
   return (
-    <div>
-      HomePage
-      <div>
-        {/* {users?.map((el) => (
-          <div key={el.id}>
-            <h2>{el.username}</h2>
-            <button onClick={handleLogout}>Logout</button>
-          </div>
-        ))} */}
-        <SideBar />
+    <div className={s.Home}>
+      <div className="container">
+        <div>
+          <MyUser />
+        </div>
       </div>
     </div>
   );
