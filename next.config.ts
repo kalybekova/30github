1e7bb46e0a9196538117bc915f939e557a9740a1
ayat kalybekova <kalybekAyat@outlook.com>
2025-02-24 16:49:25 +0600

    .

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "13.61.17.230",
+        pathname: "/media/post_img/**",
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
